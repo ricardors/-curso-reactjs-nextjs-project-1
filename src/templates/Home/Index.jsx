9b3514360e59501822789faf88412867cdd2df0b
Ricardo <ricardoropkeschneider@gmail.com>
2022-10-22 13:09:22 -0300

    Paginação de posts

--- src/templates/Home/Index.jsx
+++ src/templates/Home/Index.jsx
@@ -4,33 +4,60 @@ import './styles.css';
 
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts';
+import { Button } from '../../components/Buttom';
 
 export class Home extends Component {
 
- state = {
-      posts: []
-    };
- 
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 9,
+  };
+
 
   async componentDidMount() {
     await this.loadPosts()
-  
+
   }
 
 
-  loadPosts = async () => { 
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
   };
+
+  //load more posts
+  loadMorePosts = () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  }
+
+
 
   render() {
 
-    const { posts } = this.state;
-
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts =  page + postsPerPage >= allPosts.length? true : false;
     return (
 
       <section className='container'>
-        <Posts  posts={ posts } />
+        <Posts posts={posts} />
+        <div className='button-container' >
+          <Button text='Load more posts'
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+
+        </div>
       </section>
 
     );
