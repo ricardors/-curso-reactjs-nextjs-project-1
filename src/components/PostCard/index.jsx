0e252432aa64f994773e4920508038769f22f2a3
Ricardo Schneider <ricardoropkeschneider@gmail.com>
2022-11-28 22:31:03 -0300

    Continuação Eslint, prettier, editorconfig e prop-types

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,3 +1,4 @@
+import P from 'prop-types';
 import './styles.css';
 
 export const PostCard = ({ title, cover, body, id }) => (
@@ -11,3 +12,10 @@ export const PostCard = ({ title, cover, body, id }) => (
     </div>
   </div>
 );
+
+PostCard.propTypes = {
+  title: P.string.isRequired,
+  cover: P.string.isRequired,
+  body: P.string.isRequired,
+  id: P.number.isRequired,
+};
