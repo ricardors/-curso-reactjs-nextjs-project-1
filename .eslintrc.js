a0ccd5c83804aa8d82a3eabfa31fd79e819ced61
Ricardo Schneider <ricardo.schneider@leadforce.com.br>
2023-05-23 20:50:24 -0300

    Anotaçoes das aulas

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
